3cbcc20ccd81b1451a3baa5231553d6d9b77fce3
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-07-02 13:27:18 +0200

    first files

---
+++ door/src/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render(){
+    return (
+        <div className='bb fl w-100 b--black-05'>
+   <h3>MEGA hard techno 2020</h3>
+   <p>Ritzi Lee, David Moleon</p>
+   <p>Club Paradiso, Kerkstraat 25, Amsterdam</p>
+</div>
+    )
+}
+}
+export default Home;
